c2d97991e7caf28205dc895c67f70171ecc5bb5d
NdiranguAndrew43 <andymurunga@gmail.com>
2024-04-02 12:46:45 -0700

    made alot of code but more documentation

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-square.py by Andrew Ndirangu(2/4/24)
+"""Defining a square """
+
+class Square:
+    """an empty class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-square.py by Andrew Ndirangu(2/4/24)
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize this square class"""
+        """Args: size - represents the size of the square"""
+
+        self.__size = size
