588eac05f0fd39acd417bd3cdd891e6fe8ea08e7
NdiranguAndrew43 <andymurunga@gamil.com>
2024-04-25 04:04:51 -0700

    Created 3-main.py and 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This a moduele that will have a function to print 'My name is <first name> <last name>'
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This is a function to print the message 'My name is <first name> <last name>'.
+
+    Args:
+    first_name: this is a string passed into the function.
+    second_name: this is a string passed into the function.
+
+    Returns:
+    The message with the firstname and lastname strings attached
+    'My name is <firstname> <lastname>'
+
+    Raises:
+    TypeError: raises a TypeError if first_name or last_name are not strings
+    """
+
